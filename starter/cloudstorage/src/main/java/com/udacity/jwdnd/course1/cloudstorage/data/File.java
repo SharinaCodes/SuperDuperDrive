ad045beb243a21cf34c68cf9708500bc7ae94a8f
Sharina V. Jones <135335430+SharinaCodes@users.noreply.github.com>
2026-06-07 16:19:16 -0400

    Adding data pojos

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/data/File.java
@@ -0,0 +1,59 @@
+package com.udacity.jwdnd.course1.cloudstorage.data;
+
+public class File {
+    private Integer fileId;
+    private String fileName;
+    private String contentType;
+    private String fileSize;
+    private Integer userId;
+    /*TODO: revisit file type*/
+    private Integer fileData;
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(Integer fileData) {
+        this.fileData = fileData;
+    }
+}
